3e2ab9fde39d9b770c4699e6350b80072077feca
ktb683 <katie.ballinger@gmail.com>
2023-02-12 18:45:18 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+Notes:
+-------------------
+Components:
+- spinning mandala (first put img, then draw it myself)
+- input field expands with writing
+- sentiment analyses changes mandala to red
+- cleanse button
+  - gong sound when pressed
+  - starts setting the colour back to max green over the course of an hour
+  - small message/quote about cleansing picks one from a bunch of them
+
+Bonus:
+** small floating question mark around mandala in a bubble that will expand to
+show reasoning behind project. Small animation.
+  - if click anywhere, closes
+  - if inactive for 2 mins it closes
+*/
