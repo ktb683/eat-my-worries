927590b776f12f4da10ad7a47936cfc01d58dddc
ktb683 <katie.ballinger@gmail.com>
2023-02-17 13:50:03 +0200

    functional alpha! randomized placeholder text and cleanseButton working ✨

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ Components:
   - starts setting the colour back to max green over the course of an hour
   - small message/quote about cleansing picks one from a bunch of them
 - randomize starting messages
-- text flies away
+- text flies
+
+- speech to text
 
 Bonus:
 ** small floating question mark around mandala in a bubble that will expand to
@@ -20,11 +22,49 @@ show reasoning behind project. Small animation.
   - if click anywhere, closes
   - if inactive for 2 mins it closes
 */
-const cleanse = document.querySelector("#cleanseButton");
-guessButton.addEventListener("click", function (event) {
-  guessNumber();
+
+// list of placeholder text
+const placeHolders = [
+  "I worry...",
+  "I worry that...",
+  "I'm worried about...",
+  "I'm worried that...",
+
+  "I fear...",
+  "I fear that...",
+  "I have fear that...",
+  "I'm fearful of...",
+  "I'm fearful that...",
+
+  "I resent...",
+  "I resent that...",
+  "I'm resentful of...",
+  "I'm resentful that...",
+  "I'm resentful about..."
+];
+
+// pick a text at random
+var placeHolder = "";
+
+function pickPlaceHolder() {
+  placeHolder = placeHolders[Math.floor(Math.random()*placeHolders.length)];
+}
+
+// setup page when it loads
+start();
+
+function start() {
+  pickPlaceHolder();
+  document.getElementById("worries").textContent = placeHolder;
+}
+
+// When cleanse button is pressed, clears text and resets placeholder text
+const cleanseButton = document.querySelector(".cleanseButton");
+
+cleanseButton.addEventListener("click", function (event) {
+  cleanse();
 });
 
 function cleanse() {
-    document.getElementById("worries").value = "";
+    document.getElementById("worries").textContent = placeHolder;
 }
